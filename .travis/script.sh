0f91b708b601ede349e24b172ec6bf0eb01874ee
Mizux <corentinl@google.com>
2018-08-09 17:39:42 +0200

    Update script.sh

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -x -e
+if [ "${TRAVIS_OS_NAME}" == osx ];then
+  # Installer changes path but won't be picked up in current terminal session
+  # Need to explicitly add location
+		export PATH=/usr/local/share/dotnet:"${PATH}";
+fi
 
 dotnet --info
 
