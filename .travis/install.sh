3bf119e92984ac4b29398b8364c13826e7b63a7a
Mizux <corentinl@google.com>
2018-08-09 17:37:47 +0200

    Update install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,7 @@ set -x -e
 if [ "${TRAVIS_OS_NAME}" == linux ]; then
   # Installs for Ubuntu Trusty distro
   sudo apt-get update -q &&
-  sudo apt-get install -y -q curl gpg apt-transport-https
+  sudo apt-get install -y -q curl apt-transport-https
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &&
   sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &&
   sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list' &&
