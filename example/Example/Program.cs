de812fd3f1e438ca483326c452b57de2da429c5c
Corentin Le Molgat <corentinl@google.com>
2018-08-10 15:53:41 +0200

    Add Example depending on Foo (local) package

---
+++ example/Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Foo;
+
+namespace Example {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello from Example!");
+      Console.WriteLine("Calling Foo:...");
+      Foo.Foo.Hello();
+      Console.WriteLine("Calling Foo:...DONE");
+    }
+  }
+}
