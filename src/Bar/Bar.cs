0ec3604e42dbb06c7716c8c0e4090ab74ff3cb7d
Corentin Le Molgat <corentinl@google.com>
2018-08-16 10:29:15 +0200

    Add Bar and BarApp project

---
+++ src/Bar/Bar.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Bar {
+  public class Bar {
+    public static void StaticCall(int level) {
+      Console.WriteLine($"[{level}] Enter Bar");
+      Console.WriteLine($"[{level}] Exit Bar");
+    }
+  }
+}
---
+++ src/BarApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BarApp {
+  class Program {
+    static void Main(string[] args) {
+      int level = 1;
+      Console.WriteLine($"[{level}] Enter BarApp");
+      Bar.Bar.StaticCall(level+1);
+      Console.WriteLine($"[{level}] Exit BarApp");
+    }
+  }
+}
