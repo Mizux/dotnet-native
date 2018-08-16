1bd3b06a9babdbf0bb48ce76eadf5c071f6b772a
Corentin Le Molgat <corentinl@google.com>
2018-08-16 10:09:33 +0200

    Add Example.Native targeting Foo.linux-x64
    - not working like any example targeting Foo* packages

---
+++ example/Example.Native/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example.Native {
+  class Program {
+    static void Main(string[] args) {
+      int level = 1;
+      Console.WriteLine($"[{level}] Enter Example");
+      Foo.Native.Internal(level+1);
+      Console.WriteLine($"[{level}] Exit Example");
+    }
+  }
+}
