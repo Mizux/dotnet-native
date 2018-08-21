6ca591c122b841701c63430645c8523d84220c35
Corentin Le Molgat <corentinl@google.com>
2018-08-21 10:31:48 +0200

    Move Foo.cs to runtime package
    - Foo Project become a meta-package
      - only need one of the ref/*dll generated in runtime Project
    - each runtime Project will produce a Mizux.Foo.dll

---
+++ src/runtime.win-x64.Foo/Foo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Foo {
+    public class Foo {
+        public static void Hello(int level) {
+            Console.WriteLine($"[{level}] Enter Foo");
+            Native.Internal(level+1);
+            Console.WriteLine($"[{level}] Exit Foo");
+        }
+    }
+}
