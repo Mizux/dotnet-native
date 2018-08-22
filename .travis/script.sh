3ee2742ae0113aa8826fc604637956c340369dfc
Corentin Le Molgat <corentinl@google.com>
2018-08-22 13:51:58 +0200

    Update CI

--- .travis/script.sh
+++ .travis/script.sh
@@ -9,13 +9,17 @@ fi
 dotnet --info
 
 if [ "${TRAVIS_OS_NAME}" == linux ];then
- dotnet build --runtime linux-x64 src/FooApp/FooApp.csproj;
- dotnet src/FooApp/bin/Debug/netcoreapp2.1/linux-x64/Mizux.FooApp.dll;
- dotnet pack src/Foo.linux-x64/Foo.linux-x64.csproj -o ../../package;
- unzip -l package/Mizux.Foo.linux-x64.1.0.0.nupkg;
+  dotnet build src/runtime.linux-x64.Foo;
+  dotnet pack src/runtime.linux-x64.Foo;
 elif [ "${TRAVIS_OS_NAME}" == osx ];then
- dotnet build --runtime osx-x64 src/FooApp/FooApp.csproj;
- dotnet src/FooApp/bin/Debug/netcoreapp2.1/osx-x64/Mizux.FooApp.dll;
- dotnet pack src/Foo.osx-x64/Foo.osx-x64.csproj -o ../../package;
- unzip -l package/Mizux.Foo.osx-x64.1.0.0.nupkg;
+  dotnet build src/runtime.osx-x64.Foo;
+  dotnet pack src/runtime.osx-x64.Foo;
 fi
+dotnet build src/Foo;
+dotnet pack src/Foo;
+for i in package/*; do
+  unzip -l $i;
+done
+
+dotnet build src/FooApp;
+dotnet run --project src/FooApp;
