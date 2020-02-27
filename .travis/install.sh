fa498a1c630d8adf41471a00cfc3f57a6b4fcd8d
Mizux Seiha <mizux.dev@gmail.com>
2020-02-27 12:03:30 +0100

    Update travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -x -e
 
+function installdotnetsdk(){
+  sudo apt-get update -qq
+  sudo apt-get install -yq apt-transport-https
+  # see https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-manager-ubuntu-1604
+  wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+  sudo dpkg -i packages-microsoft-prod.deb
+  # Install dotnet sdk 3.1
+  sudo apt-get update -qq
+  sudo apt-get install -yqq dotnet-sdk-3.1
+}
+
 if [ "${TRAVIS_OS_NAME}" == linux ]; then
-  # Installs for Ubuntu Trusty distro
-  sudo apt-get update -q &&
-  sudo apt-get install -y -q curl apt-transport-https
-  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &&
-  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &&
-  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list' &&
-  sudo apt-get update -q &&
-  sudo apt-get install -y -q dotnet-sdk-2.1
+  installdotnetsdk
 elif [ "${TRAVIS_OS_NAME}" == osx ]; then
   brew update;
   brew tap caskroom/cask;
