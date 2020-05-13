e3035ba6c70fc231f6116e0fba3cf34f8c2411f9
Mizux Seiha <mizux.dev@gmail.com>
2020-05-13 12:09:20 +0200

    cmake: Replace csproj script by simpler command

--- cmake/dotnet.cmake
+++ cmake/dotnet.cmake
@@ -47,6 +47,7 @@ file(COPY dotnet/Directory.Build.props DESTINATION dotnet)
 ###############################
 ##  Mizux.Foo.runtime.<RID>  ##
 ###############################
+set(DOTNET_PACKAGES_DIR ${PROJECT_BINARY_DIR}/dotnet/packages)
 if(APPLE)
   set(RUNTIME_IDENTIFIER osx-x64)
 elseif(UNIX)
@@ -58,24 +59,22 @@ else()
 endif()
 set(DOTNET_NATIVE ${DOTNET}.runtime.${RUNTIME_IDENTIFIER})
 
-file(GENERATE OUTPUT dotnet/$<CONFIG>/replace_runtime.cmake
-  CONTENT
-  "FILE(READ ${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}.runtime.csproj.in input)
-  STRING(REPLACE \"@PROJECT_VERSION@\" \"${PROJECT_VERSION}\" input \"\${input}\")
-  STRING(REPLACE \"@RUNTIME_IDENTIFIER@\" \"${RUNTIME_IDENTIFIER}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET@\" \"${DOTNET}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET_NATIVE@\" \"${DOTNET_NATIVE}\" input \"\${input}\")
-  STRING(REPLACE \"@Foo@\" \"$<TARGET_FILE:Foo>\" input \"\${input}\")
-  STRING(REPLACE \"@native@\" \"$<TARGET_FILE:mizux-foo-native>\" input \"\${input}\")
-  FILE(WRITE ${DOTNET_NATIVE}/${DOTNET_NATIVE}.csproj \"\${input}\")"
-  )
+# pom*.xml.in contains:
+# CMake variable(s) (@PROJECT_NAME@) that configure_file() can manage and
+# generator expression ($<TARGET_FILE:...>) that file(GENERATE) can manage.
+configure_file(
+  ${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}.runtime.csproj.in
+  ${PROJECT_BINARY_DIR}/dotnet/${DOTNET}.runtime.csproj.in
+  @ONLY)
+file(GENERATE
+  OUTPUT ${PROJECT_BINARY_DIR}/dotnet/$<CONFIG>/${DOTNET}.runtime.csproj.in
+  INPUT ${PROJECT_BINARY_DIR}/dotnet/${DOTNET}.runtime.csproj.in)
 
 add_custom_command(
   OUTPUT dotnet/${DOTNET_NATIVE}/${DOTNET_NATIVE}.csproj
   COMMAND ${CMAKE_COMMAND} -E make_directory ${DOTNET_NATIVE}
-  COMMAND ${CMAKE_COMMAND} -P ./$<CONFIG>/replace_runtime.cmake
-  WORKING_DIRECTORY dotnet
-  )
+  COMMAND ${CMAKE_COMMAND} -E copy ./$<CONFIG>/${DOTNET}.runtime.csproj.in ${DOTNET_NATIVE}/${DOTNET_NATIVE}.csproj
+  WORKING_DIRECTORY dotnet)
 
 add_custom_target(dotnet_native_package
   DEPENDS
@@ -84,27 +83,21 @@ add_custom_target(dotnet_native_package
   COMMAND ${CMAKE_COMMAND} -E make_directory packages
   COMMAND ${DOTNET_EXECUTABLE} build -c Release /p:Platform=x64 ${DOTNET_NATIVE}/${DOTNET_NATIVE}.csproj
   COMMAND ${DOTNET_EXECUTABLE} pack -c Release ${DOTNET_NATIVE}/${DOTNET_NATIVE}.csproj
-  WORKING_DIRECTORY dotnet
-  )
+  WORKING_DIRECTORY dotnet)
 
 #################
 ##  Mizux.Foo  ##
 #################
-file(GENERATE OUTPUT dotnet/$<CONFIG>/replace.cmake
-  CONTENT
-  "FILE(READ ${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}.csproj.in input)
-  STRING(REPLACE \"@PROJECT_VERSION@\" \"${PROJECT_VERSION}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET@\" \"${DOTNET}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET_PACKAGES_DIR@\" \"${PROJECT_BINARY_DIR}/dotnet/packages\" input \"\${input}\")
-  FILE(WRITE ${DOTNET}/${DOTNET}.csproj \"\${input}\")"
-  )
+configure_file(
+  ${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}.csproj.in
+  ${PROJECT_BINARY_DIR}/dotnet/${DOTNET}.csproj.in
+  @ONLY)
 
 add_custom_command(
   OUTPUT dotnet/${DOTNET}/${DOTNET}.csproj
   COMMAND ${CMAKE_COMMAND} -E make_directory ${DOTNET}
-  COMMAND ${CMAKE_COMMAND} -P ./$<CONFIG>/replace.cmake
-  WORKING_DIRECTORY dotnet
-  )
+	COMMAND ${CMAKE_COMMAND} -E copy ./${DOTNET}.csproj.in ${DOTNET}/${DOTNET}.csproj
+  WORKING_DIRECTORY dotnet)
 
 add_custom_target(dotnet_package ALL
   DEPENDS
@@ -112,8 +105,7 @@ add_custom_target(dotnet_package ALL
     dotnet/${DOTNET}/${DOTNET}.csproj
   COMMAND ${DOTNET_EXECUTABLE} build -c Release /p:Platform=x64 ${DOTNET}/${DOTNET}.csproj
   COMMAND ${DOTNET_EXECUTABLE} pack -c Release ${DOTNET}/${DOTNET}.csproj
-  WORKING_DIRECTORY dotnet
-  )
+  WORKING_DIRECTORY dotnet)
 
 ######################
 ##  Mizux.FooTests  ##
@@ -122,24 +114,18 @@ add_custom_command(
   OUTPUT dotnet/${DOTNET}Tests/FooTests.cs
   COMMAND ${CMAKE_COMMAND} -E make_directory ${DOTNET}Tests
   COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/dotnet/FooTests.cs ${DOTNET}Tests/FooTests.cs
-  WORKING_DIRECTORY dotnet
-  )
+  WORKING_DIRECTORY dotnet)
 
-file(GENERATE OUTPUT dotnet/$<CONFIG>/replace_FooTests.cmake
-  CONTENT
-  "FILE(READ ${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}Tests.csproj.in input)
-  STRING(REPLACE \"@PROJECT_VERSION@\" \"${PROJECT_VERSION}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET@\" \"${DOTNET}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET_PACKAGES_DIR@\" \"${PROJECT_BINARY_DIR}/dotnet/packages\" input \"\${input}\")
-  FILE(WRITE ${DOTNET}Tests/${DOTNET}Tests.csproj \"\${input}\")"
-  )
+configure_file(
+  ${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}Tests.csproj.in
+  ${PROJECT_BINARY_DIR}/dotnet/${DOTNET}Tests.csproj.in
+  @ONLY)
 
 add_custom_command(
   OUTPUT dotnet/${DOTNET}Tests/${DOTNET}Tests.csproj
   COMMAND ${CMAKE_COMMAND} -E make_directory ${DOTNET}Tests
-  COMMAND ${CMAKE_COMMAND} -P ./$<CONFIG>/replace_FooTests.cmake
-  WORKING_DIRECTORY dotnet
-  )
+	COMMAND ${CMAKE_COMMAND} -E copy ./${DOTNET}Tests.csproj.in ${DOTNET}Tests/${DOTNET}Tests.csproj
+  WORKING_DIRECTORY dotnet)
 
 add_custom_target(FooTests ALL
   DEPENDS
@@ -147,8 +133,7 @@ add_custom_target(FooTests ALL
     dotnet/${DOTNET}Tests/FooTests.cs
     dotnet/${DOTNET}Tests/${DOTNET}Tests.csproj
   COMMAND ${DOTNET_EXECUTABLE} build -c Release /p:Platform=x64 ${DOTNET}Tests/${DOTNET}Tests.csproj
-  WORKING_DIRECTORY dotnet
-  )
+  WORKING_DIRECTORY dotnet)
 
 if(BUILD_TESTING)
   add_test(NAME FooTestsUT
@@ -163,24 +148,18 @@ add_custom_command(
   OUTPUT dotnet/${DOTNET}App/FooApp.cs
   COMMAND ${CMAKE_COMMAND} -E make_directory ${DOTNET}App
   COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/dotnet/FooApp.cs ${DOTNET}App/FooApp.cs
-  WORKING_DIRECTORY dotnet
-  )
+  WORKING_DIRECTORY dotnet)
 
-file(GENERATE OUTPUT dotnet/$<CONFIG>/replace_FooApp.cmake
-  CONTENT
-  "FILE(READ ${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}App.csproj.in input)
-  STRING(REPLACE \"@PROJECT_VERSION@\" \"${PROJECT_VERSION}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET@\" \"${DOTNET}\" input \"\${input}\")
-  STRING(REPLACE \"@DOTNET_PACKAGES_DIR@\" \"${PROJECT_BINARY_DIR}/dotnet/packages\" input \"\${input}\")
-  FILE(WRITE ${DOTNET}App/${DOTNET}App.csproj \"\${input}\")"
-  )
+configure_file(
+	${PROJECT_SOURCE_DIR}/dotnet/${DOTNET}App.csproj.in
+	${PROJECT_BINARY_DIR}/dotnet/${DOTNET}App.csproj.in
+  @ONLY)
 
 add_custom_command(
   OUTPUT dotnet/${DOTNET}App/${DOTNET}App.csproj
   COMMAND ${CMAKE_COMMAND} -E make_directory ${DOTNET}App
-  COMMAND ${CMAKE_COMMAND} -P ./$<CONFIG>/replace_FooApp.cmake
-  WORKING_DIRECTORY dotnet
-  )
+	COMMAND ${CMAKE_COMMAND} -E copy ./${DOTNET}App.csproj.in ${DOTNET}App/${DOTNET}App.csproj
+  WORKING_DIRECTORY dotnet)
 
 add_custom_target(FooApp ALL
   DEPENDS
@@ -188,8 +167,7 @@ add_custom_target(FooApp ALL
     dotnet/${DOTNET}App/FooApp.cs
     dotnet/${DOTNET}App/${DOTNET}App.csproj
   COMMAND ${DOTNET_EXECUTABLE} build -c Release /p:Platform=x64 ${DOTNET}App/${DOTNET}App.csproj
-  WORKING_DIRECTORY dotnet
-  )
+  WORKING_DIRECTORY dotnet)
 
 if(BUILD_TESTING)
   add_test(NAME FooAppUT
