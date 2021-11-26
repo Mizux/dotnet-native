893bf1651109e81723b5ea5f019ca09408ce2b37
Corentin Le Molgat <corentinl@google.com>
2021-11-26 15:54:45 +0100

    Add tests

---
+++ tests/FooBarTests.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+using Mizux.DotnetNative;
+
+namespace Mizux.DotnetNative.Tests {
+  public class FooBarTest {
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void IntegerTest(bool callGC) {
+      // Instantiate FooBar
+      FooBar obj = new FooBar();
+
+      if (callGC) {
+        GC.Collect();
+      }
+
+      obj.SetBarInt(3);
+      obj.SetFooInt(5);
+      Assert.Equal(8, obj.GetInt());
+
+      obj.SetBarInt(11);
+      obj.SetFooInt(31);
+      Assert.Equal(42, obj.GetInt());
+    }
+
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void Integer64Test(bool callGC) {
+      // Instantiate FooBar
+      FooBar obj = new FooBar();
+
+      if (callGC) {
+        GC.Collect();
+      }
+
+      long a = 2147483647;
+      long b = 2147483648;
+      obj.SetBarInt64(a);
+      obj.SetFooInt64(b);
+      Assert.Equal(a+b, obj.GetInt64());
+    }
+  }
+}
