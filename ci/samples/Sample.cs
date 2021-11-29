7dac96f31614bc33ba8a138ac058c3a8e50a06ac
Corentin Le Molgat <corentinl@google.com>
2021-11-29 10:00:18 +0100

    ci: Rework Sample.cs

--- ci/samples/Sample.cs
+++ ci/samples/Sample.cs
@@ -1,13 +1,25 @@
 using System;
-using Mizux.DotnetNative;
+using Mizux.DotnetNative.Bar;
+using Mizux.DotnetNative.Foo;
+using Mizux.DotnetNative.FooBar;
 
-namespace Mizux.DotnetNative.Sample {
+namespace Mizux.DotnetNative.FooApp {
   class Program {
     static void Main(string[] args) {
       int level = 1;
-      Console.WriteLine($"[{level}] Enter FooApp");
-      Foo.StaticFunction(level+1);
-      Console.WriteLine($"[{level}] Exit FooApp");
+      Console.WriteLine($"[{level}] Enter Sample");
+      Bar.Bar.StaticFunction(level+1);
+      Console.WriteLine($"[{level}] Exit Sample");
+
+      level = 1;
+      Console.WriteLine($"[{level}] Enter Sample");
+      Foo.Foo.StaticFunction(level+1);
+      Console.WriteLine($"[{level}] Exit Sample");
+
+      level = 1;
+      Console.WriteLine($"[{level}] Enter Sample");
+      FooBar.FooBar.StaticFunction(level+1);
+      Console.WriteLine($"[{level}] Exit Sample");
     }
   }
 }
